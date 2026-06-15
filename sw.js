4c2ba96b02415a8b2689e3f24c02a05177d696bf
1731664668616656467367 <17325917904@163.com>
2026-06-16 02:31:36 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = "expense-cache-v1";
+const URLS = ["/", "/manifest.json", "/icon-192.png", "/icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(URLS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
